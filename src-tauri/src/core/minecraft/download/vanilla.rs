f00b0d7ae0919738214bf8d8cb8f8cb58f627927
tangge233 <tangge233@qq.com>
2025-01-31 23:39:57 +0800

    feat: version download test

--- src-tauri/src/core/minecraft/download/vanilla.rs
+++ src-tauri/src/core/minecraft/download/vanilla.rs
@@ -1,15 +1,28 @@
 
 use std::error::Error;
-
+use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 use serde_json;
 
-use crate::core::utils::net;
+use crate::core::utils::{net,downloader};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct VersionManifestOverall {
     pub latest: VersionManifestLatest,
     pub versions: Vec<VersionManifest>,
+}
+
+impl VersionManifestOverall {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let client = net::HttpClient::new();
+        match client.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").await {
+            Ok(data) => {
+                let list: VersionManifestOverall = serde_json::from_str(&data.body).unwrap();
+                Ok(list)
+            }
+            Err(e) => Err(e)
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -27,6 +40,28 @@ pub struct VersionManifest {
     pub time: String,
     #[serde(rename = "releaseTime")]
     pub release_time: String,
+}
+
+impl VersionManifest {
+    pub async fn download(&self,dest: PathBuf) -> Result<(), Box<dyn Error>> {
+        // 下载实例 Json 文件
+        let _temp = PathBuf::from(&self.url);
+        let filename = match _temp.file_name() {
+            Some(name) => name,
+            None => return Err(Box::from("No filename given"))
+        };
+        let config =vec![downloader::DownloadManagerConfig{
+            url: self.url.clone(),
+            dest: dest.join(filename),
+            max_threads:1,
+            max_retries:5,
+            timeout_secs:30,
+        }];
+        println!("Downloading {:?}",self);
+        let dl = downloader::DownloadManager::new(&config).unwrap();
+        dl.download_all().await;
+        Ok(())
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
@@ -76,11 +111,4 @@ impl DownloadSource {
             Self::BMCLAPI => "https://bmclapi2.bangbang93.com/mc/game/".to_string() + url_path
         }
     }
-}
-
-pub async fn get_manifest_version_list() -> Result<VersionManifestOverall, Box<dyn Error>> {
-    let client = net::HttpClient::new();
-    let response =  client.get("https://launchermeta.mojang.com/mc/game/version_manifest.json").await.unwrap();
-    let list: VersionManifestOverall = serde_json::from_str(&response.body).unwrap();
-    Ok(list)
 }
--- src-tauri/tests/minecraft.rs
+++ src-tauri/tests/minecraft.rs
@@ -4,9 +4,23 @@ use pcl2_nova_app_lib::core::minecraft;
 
 #[test]
 fn version_list_get_test(){
-    let res = tokio::runtime::Runtime::new().unwrap().block_on(minecraft::download::vanilla::get_manifest_version_list());
+    let res = tokio::runtime::Runtime::new().unwrap().block_on(minecraft::download::vanilla::VersionManifestOverall::new());
     match res {
         Ok(v) => println!("{:?}", v),
         Err(e) => println!("Error: {}", e)
     }
+}
+
+#[test]
+fn version_download_test(){
+    tokio::runtime::Runtime::new().unwrap().block_on(async {
+        let res = minecraft::download::vanilla::VersionManifestOverall::new().await;
+        match res {
+            Ok(v) => {
+                let latest = v.versions.get(0).unwrap();
+                latest.download(dirs_next::desktop_dir().unwrap()).await.unwrap();
+            }
+            Err(e) => println!("Error: {}", e)
+        }
+    });
 }
