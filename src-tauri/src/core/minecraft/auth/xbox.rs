72dc698d48f96dfb4fff5cb106b9029eb976f892
shenjack <3695888@qq.com>
2025-02-06 20:24:43 +0800

    进行一个阳间改造

--- src-tauri/src/core/minecraft/auth/xbox.rs
+++ src-tauri/src/core/minecraft/auth/xbox.rs
@@ -1,13 +1,21 @@
+const XBOX_LIVE_AUTH_URL: &str = "https://user.auth.xboxlive.com/user/authenticate";
+
 pub async fn xbox_live_auth(access_token: &String) -> anyhow::Result<()> {
-    let request_url = "https://user.auth.xboxlive.com/user/authenticate".to_string();
-    let request_body =  "{\"Properties\":{\"AuthMethod\": \"RPS\",\"SiteName\": \"user.auth.xboxlive.com\",\"RpsTicket\": \"d=".to_string()
-              + access_token + "\"},\"RelyingParty\": \"http://auth.xboxlive.com\",\"TokenType\": \"JWT\"}";
+    let request_body = format!(
+        r#"{{"Properties":{{"AuthMethod":"RPS","SiteName":"user.auth.xboxlive.com","RpsTicket":"d={}"}}, "RelyingParty": "http://auth.xboxlive.com", "TokenType": "JWT"}}"#,
+        access_token
+    );
+    
     let http_client = reqwest::Client::new();
     let request = http_client
-        .post(request_url)
+        .post(XBOX_LIVE_AUTH_URL)
         .body(request_body)
         .header(reqwest::header::CONTENT_TYPE, "application/json")
         .header(reqwest::header::ACCEPT, "application/json");
-    let _ = request.send().await?;
+    let back = request.send().await?;
+    // 检查返回的状态码
+    if !back.status().is_success() {
+        return Err(anyhow::anyhow!("Failed to authenticate with Xbox Live, status code: {}", back.status()));
+    }
     Ok(())
 }
