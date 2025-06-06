7d047d83d032300d212c3a50b065d1a542c709fa
Xphost <xphost@foxmail.com>
2025-06-06 18:02:48 +0800

    Change My Select Card ui Conpoment!!

--- frontend/src/components/card/MySelectCard.vue
+++ frontend/src/components/card/MySelectCard.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import {ref, watch} from 'vue'
+import {onMounted, onUnmounted, ref, watch} from 'vue'
 import {dark_mode} from '../../logic/changeBody'
 
 interface CheckButtonProps {
   isExpand: boolean,
-  maxHeight: number,
+  // maxHeight: number,
   title: string,
   isLast?: boolean,
 }
@@ -17,36 +17,53 @@ watch(dark_mode, value => {
 })
 const isCheckedProps = withDefaults(defineProps<CheckButtonProps>(), {
   isExpand: false,
-  maxHeight: 0,
+  // maxHeight: 0,
   title: "",
   isLast: false
 })
-const mh = ref(isCheckedProps.maxHeight + 'px')
-watch(() => isCheckedProps.maxHeight, value => mh.value = value + 'px')
+// const mh = ref(isCheckedProps.maxHeight + 'px')
+const cardHeight = ref("43px")
+// watch(() => isCheckedProps.maxHeight, value => mh.value = value + 'px')
 const isExpandComp = ref(!isCheckedProps.isExpand)
-const border = ref("6px")
+// const border = ref("6px")
 const isLastComp = ref(isCheckedProps.isLast ? "15px" : "0")
 const touchColor = ref(dark_mode.value ? '#0a0a0a' : '#d6d6d6')
 
+const myCardInnerRef = ref<HTMLElement>()
+// let observer: ResizeObserver | null = null
+
 function style_cancel() {
-  border.value = isExpandComp.value ? "0" : "6px"
+  // border.value = isExpandComp.value ? "0" : "6px"
   let hovColor = dark_mode.value ? "#0a0a0a" : "#d6d6d6"
   let Color = dark_mode.value ? "#151515" : "#f8f8f8"
   touchColor.value = isExpandComp.value ? Color : hovColor
 }
 
 function changeProps() {
-  if (!isCheckedProps.isExpand) {
-    return
-  }
+  if (!isCheckedProps.isExpand) return
   isExpandComp.value = !isExpandComp.value
-  border.value = "0"
   touchColor.value = dark_mode.value ? "#151515" : "#f8f8f8"
+  cardHeight.value = isExpandComp.value ? (myCardInnerRef.value!.offsetHeight + 43) + "px" : "43px"
 }
+onMounted(() => {
+  if (!isCheckedProps.isExpand) {
+    cardHeight.value = (myCardInnerRef.value!.offsetHeight + 43) + "px"
+  }
+})
+// onMounted(() => {
+//   observer = new ResizeObserver(() => {
+//     if(!isExpandComp.value) return
+//     cardHeight.value = myCardInnerRef.value!.offsetHeight + 26
+//   })
+//   observer.observe(myCardInnerRef.value!)
+// })
+//
+// onUnmounted(() => observer?.disconnect())
+
 </script>
 <template>
-  <div>
-    <div :class="'grid' + (isCheckedProps.isExpand ? ' cursor-pointer' : '')"
+  <div class="card-container">
+    <div :class="['grid', isCheckedProps.isExpand ? ' cursor-pointer' : '']"
          :isOpen="(isExpandComp ? 'expand' : 'close')" @click="changeProps">
       {{ isCheckedProps.title }}
       <svg
@@ -61,19 +78,25 @@ function changeProps() {
         <polyline points="6 10 12 16 18 10"/>
       </svg>
     </div>
-    <div @transitionend="style_cancel()">
+    <div @transitionend="style_cancel()" ref="myCardInnerRef">
       <slot/>
     </div>
   </div>
 </template>
 <style scoped>
+.card-container {
+  flex-shrink: 0;
+  height: v-bind(cardHeight);
+  border-radius: 6px;
+  transition: height 0.2s;
+  margin: 15px 22px v-bind(isLastComp) 22px;
+  background-color: v-bind(dark);
+  overflow: hidden;
+}
 .grid[isOpen="close"],
 .grid[isOpen="expand"] {
-  margin-top: 15px;
-  background-color: v-bind(dark);
-  margin-left: 22px;
-  margin-right: 22px;
-  border-radius: 6px 6px v-bind(border) v-bind(border);
+  /* border-radius: 6px 6px v-bind(border) v-bind(border); */
+  border-radius: 6px;
   padding: 10px 20px;
   font-size: 16px;
   font-weight: bold;
@@ -86,18 +109,17 @@ function changeProps() {
 }
 
 .grid + div {
-  max-height: 0;
-  overflow: hidden;
-  border-bottom-left-radius: 6px;
-  border-bottom-right-radius: 6px;
-  margin-left: 22px;
-  margin-right: 22px;
-  background-color: v-bind(dark);
+  /* max-height: 0; */
+  /* overflow: hidden; */
+  /* border-bottom-left-radius: 6px; */
+  /* border-bottom-right-radius: 6px; */
+  /* margin-left: 22px; */
+  /* margin-right: 22px; */
+  /* background-color: v-bind(dark); */
   transition: all 0.2s;
-  margin-bottom: v-bind(isLastComp);
   color: v-bind(light);
+  overflow: hidden;
 }
-
 .card-icon,
 .card-icon-expand {
   width: 20px;
@@ -113,6 +135,7 @@ function changeProps() {
 }
 
 .grid[isOpen="expand"] + div {
-  max-height: v-bind(mh);
+  height: fit-content;
+  /* max-height: v-bind(mh); */
 }
 </style>
--- frontend/src/views/about/content/TreasureBox.vue
+++ frontend/src/views/about/content/TreasureBox.vue
@@ -4,83 +4,146 @@ import MySelectCard from '../../../components/card/MySelectCard.vue'
 <template>
   <div style="display: flex; flex-direction: column;">
     <MySelectCard :maxHeight="1100" :isExpand="true" :title="'百宝箱'" :isLast="true">
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
-      <div style="padding: 10px">
-        <center>测试：可以合上的卡片~</center>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
+      </div>
+      <div style="padding: 10px; text-align: center">
+        测试：可以合上的卡片~
       </div>
     </MySelectCard>
   </div>
